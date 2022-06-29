b8ea41f96716114b58ab91b22b950ba1d52e8f6c
Doug Red <douglasred63@yahoo.com>
2022-06-28 22:57:28 -0500

    dynamically added columns and rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,15 @@ function simpleCalendar() {
       return currentDay;
     }
     currentDay();
+
+    for (i = 0; i < workHours.length; i++) {
+      var row = $("<div>").addClass("row");
+      var timeCol = $("<p>").text(workHours[i]).addClass("hour");
+      var activityCol = $("<input>").addClass("toDO-input time-block");
+      saveBtn = $("<button>").addClass("btn saveBtn").text("save");
+      $(row).append(timeCol).append(activityCol).append(saveBtn);
+      $("#calendar").append(row);
+    }
   });
 }
 simpleCalendar();
