ebbc84dfa1bfdec2ad0a2c5fa18afc3a7a3ecb50
Doug Red <douglasred63@yahoo.com>
2022-06-28 23:40:41 -0500

    gave input field and save button unique Id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,27 @@ function simpleCalendar() {
     for (i = 0; i < workHours.length; i++) {
       var row = $("<div>").addClass("row");
       var timeCol = $("<p>").text(workHours[i]).addClass("hour");
-      var activityCol = $("<input>").addClass("toDO-input time-block");
-      saveBtn = $("<button>").addClass("btn saveBtn").text("save");
+      var activityCol = $("<input>").addClass("toDo-input time-block");
+      saveBtn = $("<button>").addClass("btn saveBtn").text("💾");
       $(row).append(timeCol).append(activityCol).append(saveBtn);
       $("#calendar").append(row);
     }
+
+    function addActivityId() {
+      var activityId = document.getElementsByClassName("toDo-input");
+      for (i = 0; i < activityId.length; i++) {
+        activityId[i].id = "activity-field-" + (i + 1);
+      }
+    }
+    addActivityId();
+
+    function addSaveBtnId() {
+      var saveBtnId = document.getElementsByClassName("saveBtn");
+      for (i = 0; i < saveBtnId.length; i++) {
+        saveBtnId[i].id = "saveBtn-" + (i + 1);
+      }
+    }
+    addSaveBtnId();
   });
 }
 simpleCalendar();
