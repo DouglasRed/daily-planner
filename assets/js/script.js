2a41fa389da0cd5ce608667d2ae491bfe7857c20
Doug Red <douglasred63@yahoo.com>
2022-06-29 21:51:24 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 function simpleCalendar() {
   $(document).ready(function () {
     var currentTime = moment().format("dddd, MMMM Do, YYYY h:mm a");
+    //This hours array will populate the columns array
     var workHours = [
       "8am",
       "9am",
@@ -14,12 +15,14 @@ function simpleCalendar() {
       "5pm",
     ];
 
+    //Calls and display current time
     function currentDay() {
       var currentDay = $("#currentDay").text(currentTime);
       return currentDay;
     }
     currentDay();
 
+    //creates the rows with classes dynamically
     for (i = 0; i < workHours.length; i++) {
       var row = $("<div>").addClass("row");
       var timeCol = $("<p>").text(workHours[i]).addClass("hour");
@@ -29,13 +32,13 @@ function simpleCalendar() {
       $("#calendar").append(row);
     }
 
+    //gives the middle column an ID that will be referenced quite frequently
     function addActivityId() {
       var activityId = document.getElementsByClassName("activity-input");
       for (i = 0; i < activityId.length; i++) {
         activityId[i].id = "activity-field-" + (i + 1);
         activityId[i].hour = i + 8;
       }
-      console.dir(activityId);
     }
     addActivityId();
 
@@ -71,12 +74,14 @@ function simpleCalendar() {
 
     var storeActivity;
 
+    //saves text content to local storage
     //Button 1
     $(saveBtnId1).on("click", function (event) {
       event.preventDefault();
       var activityId1 = $("#activity-field-1").val().trim();
       localStorage.setItem("activity-1", activityId1);
     });
+    // retrieves text from local storage if any exist
     var saveActivity1 = localStorage.getItem("activity-1");
     activityId1.val(saveActivity1);
 
@@ -162,13 +167,85 @@ function simpleCalendar() {
     activityId10.val(saveActivity10);
 
     let now = new Date().getHours();
-
+    // somewhat dynamically checks to see verify which class should be attached
     if (now > 8) {
       $("#activity-field-1").addClass("past");
     } else if (now >= 8 && now < 9) {
       $("#activity-field-1").addClass("present");
     } else if (now < 8) {
       $("#activity-field-1").addClass("future");
+    }
+
+    if (now > 9) {
+      $("#activity-field-2").addClass("past");
+    } else if (now >= 9 && now < 10) {
+      $("#activity-field-2").addClass("present");
+    } else if (now < 9) {
+      $("#activity-field-2").addClass("future");
+    }
+
+    if (now > 10) {
+      $("#activity-field-3").addClass("past");
+    } else if (now >= 10 && now < 11) {
+      $("#activity-field-3").addClass("present");
+    } else if (now < 10) {
+      $("#activity-field-3").addClass("future");
+    }
+
+    if (now > 11) {
+      $("#activity-field-4").addClass("past");
+    } else if (now >= 11 && now < 12) {
+      $("#activity-field-4").addClass("present");
+    } else if (now < 11) {
+      $("#activity-field-4").addClass("future");
+    }
+
+    if (now > 12) {
+      $("#activity-field-5").addClass("past");
+    } else if (now >= 12 && now < 13) {
+      $("#activity-field-5").addClass("present");
+    } else if (now < 12) {
+      $("#activity-field-5").addClass("future");
+    }
+
+    if (now > 13) {
+      $("#activity-field-6").addClass("past");
+    } else if (now >= 13 && now < 14) {
+      $("#activity-field-6").addClass("present");
+    } else if (now < 13) {
+      $("#activity-field-6").addClass("future");
+    }
+
+    if (now > 14) {
+      $("#activity-field-7").addClass("past");
+    } else if (now >= 14 && now < 15) {
+      $("#activity-field-7").addClass("present");
+    } else if (now < 14) {
+      $("#activity-field-7").addClass("future");
+    }
+
+    if (now > 15) {
+      $("#activity-field-8").addClass("past");
+    } else if (now >= 15 && now < 16) {
+      $("#activity-field-8").addClass("present");
+    } else if (now < 15) {
+      $("#activity-field-8").addClass("future");
+    }
+
+    if (now > 16) {
+      $("#activity-field-9").addClass("past");
+    } else if (now >= 16 && now < 17) {
+      $("#activity-field-9").addClass("present");
+    } else if (now < 16) {
+      $("#activity-field-9").addClass("future");
+    }
+
+    if (now > 17) {
+      $("#activity-field-10").addClass("past");
+    } else if (now >= 17 && now < 18) {
+      $("#activity-field-10").addClass("present");
+    } else if (now < 17) {
+      $("#activity-field-10").addClass("future");
     }
   });
 }
